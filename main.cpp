d61c0cea013c287e77d45d755f67d327b1924a82
Alin Cristian <alin.cristian977@gmail.com>
2023-09-15 00:07:50 +0300

    comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
+  // This is a comment
+
+  /*
+    This is a multi-line comment
+  */
+
   std::cout << "Hello World!\n";
   std::cout << "Bye World!";
   std::cout << "Number 3" << std::endl;
