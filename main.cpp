91068ccf0fc718e17f88d6dda287396bfb262a32
Alin Cristian <alin.cristian977@gmail.com>
2023-09-15 00:05:57 +0300

    writing more statements

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello World!\n";
+  std::cout << "Bye World!";
+  std::cout << "Number 3" << std::endl;
+
   return 0;
 }
