2642cc2fec7485e6991593daa5044b9872cb6c8c
Alin Cristian <alin.cristian977@gmail.com>
2023-09-17 15:15:09 +0300

    Number Systems

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-// Variables and Data Types
+// Number Systems
 
 #include <iostream>
 
 int main() {
-  
+  int numberDecimal = 15; // Decimal
+  int numberOctal = 017; // Octal
+  int numberHexadecimal = 0x0f; // Hexadecimal
+  int numberBinary = 0b00001111; // Binary
+
+  std::cout << "numberDecimal: " << numberDecimal << std::endl;
+  std::cout << "numberOctal: " << numberOctal << std::endl;
+  std::cout << "numberHexadecimal: " << numberHexadecimal << std::endl;
+  std::cout << "numberBinary: " << numberBinary << std::endl;
 
   return 0;
 }
