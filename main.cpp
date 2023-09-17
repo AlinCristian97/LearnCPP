9cf8a99cf72c1b76c3e6d0f2f90959a282e6b1d4
Alin Cristian <alin.cristian977@gmail.com>
2023-09-17 12:53:54 +0300

    compile-time and run-time errors

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
 int main() {
-  // This is a comment
+  // Compile-time error
+  // std::cout << "Hello World!" << std::endl // Uncomment this line to see the compile-time error in effect
 
-  /*
-    This is a multi-line comment
-  */
-
-  std::cout << "Hello World!\n";
-  std::cout << "Bye World!";
-  std::cout << "Number 3" << std::endl;
+  // Run-time error
+  int value = 7/0;
+  std::cout << "value: " << value << std::endl;
 
   return 0;
 }
