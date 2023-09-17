796319d5d6d6afaa57af1918597f171c263a60b1
Alin Cristian <alin.cristian977@gmail.com>
2023-09-17 15:42:17 +0300

    Integers

--- main.cpp
+++ main.cpp
@@ -1,17 +1,60 @@
-// Number Systems
+// Integers
 
 #include <iostream>
 
 int main() {
-  int numberDecimal = 15; // Decimal
-  int numberOctal = 017; // Octal
-  int numberHexadecimal = 0x0f; // Hexadecimal
-  int numberBinary = 0b00001111; // Binary
 
-  std::cout << "numberDecimal: " << numberDecimal << std::endl;
-  std::cout << "numberOctal: " << numberOctal << std::endl;
-  std::cout << "numberHexadecimal: " << numberHexadecimal << std::endl;
-  std::cout << "numberBinary: " << numberBinary << std::endl;
+  // // Braced initialization
+
+  // int elephantCount; // Variable may contain random garbage value. WARNING
+
+  // int lionCount{}; // Initializes to 0
+
+  // int dogCount{10};
+
+  // int catCount{15};
+
+  // int domesticatedAnimals {dogCount + catCount}; // Can use expression as initializer
+
+  // // int narrowingConversion {2.9}; // Information Lost. WARNING OR ERROR
+
+  // std::cout << "Elephant count: " << elephantCount << std::endl;
+  // std::cout << "Lion count: " << lionCount << std::endl;
+  // std::cout << "Dog count: " << dogCount << std::endl;
+  // std::cout << "Cat count: " << catCount << std::endl;
+  // std::cout << "Domesticated Animals count: " << domesticatedAnimals << std::endl;
+
+  // // Functional initialization
+
+  // int appleCount(5);
+  // int orangeCount(10);
+  // int fruitCount(appleCount + orangeCount);
+
+  // // int badInitialization(doesnt_exist + doesnt_exist);
+
+  // int narrowingConversion(2.9); // Information Lost. Less safe than braced intialization
+
+  // std::cout << "Apple count: " << appleCount << std::endl;
+  // std::cout << "Orange count: " << orangeCount << std::endl;
+  // std::cout << "Fruit count: " << fruitCount << std::endl;
+  // std::cout << "Narrowing Conversion: " << narrowingConversion << std::endl;
+
+  // Assignment Initialization
+
+  int bikeCount = 2;
+  int truckCount = 7;
+  int vehicleCount = bikeCount + truckCount;
+  int narrowingConversion = 2.9;
+
+  std::cout << "Bike count: " << bikeCount << std::endl;
+  std::cout << "Truck count: " << truckCount << std::endl;
+  std::cout << "Vehicle count: " << vehicleCount << std::endl;
+  std::cout << "Narrowing Conversion: " << narrowingConversion << std::endl;
+
+  // Check the size with sizeof
+
+  std::cout << "sizeof int: " << sizeof(int) << std::endl;
+  std::cout << "sizeof truckCount: " << sizeof(truckCount) << std::endl;
 
   return 0;
 }
