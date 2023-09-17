930d67a134ac69893c43d32fcf48a98a2cb764ea
Alin Cristian <alin.cristian977@gmail.com>
2023-09-17 13:54:10 +0300

    Statements and Functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
+// Statements and Functions
+
 #include <iostream>
 
-int main() {
-  // Compile-time error
-  // std::cout << "Hello World!" << std::endl // Uncomment this line to see the compile-time error in effect
+int addNumbers(int firstParam, int secondParam) {
+  int result = firstParam + secondParam;
+  return result;
+}
 
-  // Run-time error
-  int value = 7/0;
-  std::cout << "value: " << value << std::endl;
+int main() {
+
+  int firstNumber {3}; // Statement
+  int secondNumber = 7;
+
+  std::cout << "First number: " << firstNumber << std::endl;
+  std::cout << "Second number: " << secondNumber << std::endl;
+
+  int sum = firstNumber + secondNumber;
+  std::cout << "Direct Sum: " << sum << std::endl;
+
+  sum = addNumbers(25, 5);
+  std::cout << "Function Sum: " << sum << std::endl;
 
   return 0;
 }
