4a200f5ad9ccb5869ca895a286147c92dcf37625
Alin Cristian <alin.cristian977@gmail.com>
2023-09-17 17:56:06 +0300

    Integer Modifiers

--- main.cpp
+++ main.cpp
@@ -1,60 +1,19 @@
-// Integers
+// Integer Modifiers
 
 #include <iostream>
 
 int main() {
+  
+  signed int value1 {10};
+  signed int value2 {-300};
 
-  // // Braced initialization
+  std::cout << "value1: " << value1 << std::endl;
+  std::cout << "value2: " << value2 << std::endl;
+  std::cout << "sizeof(value1): " << sizeof(value1) << std::endl;
+  std::cout << "sizeof(value2): " << sizeof(value2) << std::endl;
 
-  // int elephantCount; // Variable may contain random garbage value. WARNING
-
-  // int lionCount{}; // Initializes to 0
-
-  // int dogCount{10};
-
-  // int catCount{15};
-
-  // int domesticatedAnimals {dogCount + catCount}; // Can use expression as initializer
-
-  // // int narrowingConversion {2.9}; // Information Lost. WARNING OR ERROR
-
-  // std::cout << "Elephant count: " << elephantCount << std::endl;
-  // std::cout << "Lion count: " << lionCount << std::endl;
-  // std::cout << "Dog count: " << dogCount << std::endl;
-  // std::cout << "Cat count: " << catCount << std::endl;
-  // std::cout << "Domesticated Animals count: " << domesticatedAnimals << std::endl;
-
-  // // Functional initialization
-
-  // int appleCount(5);
-  // int orangeCount(10);
-  // int fruitCount(appleCount + orangeCount);
-
-  // // int badInitialization(doesnt_exist + doesnt_exist);
-
-  // int narrowingConversion(2.9); // Information Lost. Less safe than braced intialization
-
-  // std::cout << "Apple count: " << appleCount << std::endl;
-  // std::cout << "Orange count: " << orangeCount << std::endl;
-  // std::cout << "Fruit count: " << fruitCount << std::endl;
-  // std::cout << "Narrowing Conversion: " << narrowingConversion << std::endl;
-
-  // Assignment Initialization
-
-  int bikeCount = 2;
-  int truckCount = 7;
-  int vehicleCount = bikeCount + truckCount;
-  int narrowingConversion = 2.9;
-
-  std::cout << "Bike count: " << bikeCount << std::endl;
-  std::cout << "Truck count: " << truckCount << std::endl;
-  std::cout << "Vehicle count: " << vehicleCount << std::endl;
-  std::cout << "Narrowing Conversion: " << narrowingConversion << std::endl;
-
-  // Check the size with sizeof
-
-  std::cout << "sizeof int: " << sizeof(int) << std::endl;
-  std::cout << "sizeof truckCount: " << sizeof(truckCount) << std::endl;
+  unsigned int value3 {4};
+  // unsigned int value4 {-5}; // Compile-time error
 
   return 0;
 }
