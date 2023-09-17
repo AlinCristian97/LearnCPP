849e474ee54549dd6728235c1a6d22da6b6f3d95
Alin Cristian <alin.cristian977@gmail.com>
2023-09-18 00:52:34 +0300

    Assignments

--- main.cpp
+++ main.cpp
@@ -1,28 +1,45 @@
-// Auto
+// Assignments
 
 #include <iostream>
 
 int main() {
 
-	auto var1 {12};
-  auto var2 {13.0};
-  auto var3 {14.0f};
-  auto var4 {15.0l};
-  auto var5 {'e'};
+	int var1{123}; // Declare and initialize
+  std::cout << "var1 : "  << var1 << std::endl;
   
-  //int modifier suffixes
-  auto var6 { 123u}; // unsigned
-  auto var7 { 123ul}; //unsigned long
-  auto var8 { 123ll}; // long long
+  var1 = 55; // Assign
+  std::cout << "var1 : "  << var1 << std::endl;
+  
+  std::cout << std::endl;
 
-  std::cout << "var1 occupies : " << sizeof(var1) << " bytes" << std::endl;
-  std::cout << "var2 occupies : " << sizeof(var2) << " bytes" << std::endl;
-  std::cout << "var3 occupies : " << sizeof(var3) << " bytes" << std::endl;
-  std::cout << "var4 occupies : " << sizeof(var4) << " bytes" << std::endl;
-  std::cout << "var5 occupies : " << sizeof(var5) << " bytes" << std::endl;
-  std::cout << "var6 occupies : " << sizeof(var6) << " bytes" << std::endl;
-  std::cout << "var7 occupies : " << sizeof(var7) << " bytes" << std::endl;
-  std::cout << "var8 occupies : " << sizeof(var8) << " bytes" << std::endl;
+  std::cout << "----------------" << std::endl;
+
+  double var2 {44.55}; // Declare and initialize
+  std::cout << "var2 : " << var2 << std::endl;
+  
+  var2 = 99.99; // Assign
+  std::cout << "var2 : " << var2 << std::endl;
+  
+  std::cout << std::endl;
+
+  std::cout << "----------------" << std::endl;
+
+  std::cout << std::endl;
+  bool state{false}; // Declare and initialize
+  std::cout << std::boolalpha;
+  std::cout << "state : " << state << std::endl;
+  
+  state = true; // Assign
+  
+  std::cout << "state : " << state << std::endl;
+
+  //Auto type deduction
+  //Careful about auto assignments
+  auto var3 {333u}; // Declare and initialize with type deduction
+  
+  var3 = -22; // Assign negative number. DANGER!
+  
+  std::cout << "var3 : " << var3 << std::endl;
 
   return 0;
 }
