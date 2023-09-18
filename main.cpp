6f05992f28db66d1bca11e960a95f4b8aa480ac9
Alin Cristian <alin.cristian977@gmail.com>
2023-09-18 10:15:38 +0300

    Basic Operations

--- main.cpp
+++ main.cpp
@@ -1,45 +1,39 @@
-// Assignments
+// Basic Operations
 
 #include <iostream>
 
 int main() {
 
-	int var1{123}; // Declare and initialize
-  std::cout << "var1 : "  << var1 << std::endl;
-  
-  var1 = 55; // Assign
-  std::cout << "var1 : "  << var1 << std::endl;
-  
-  std::cout << std::endl;
+  //Addition
+  int number1 {2};
+  int number2 {7};
 
-  std::cout << "----------------" << std::endl;
+  int result = number1 + number2;
+  std::cout << "result : " << result << std::endl;
 
-  double var2 {44.55}; // Declare and initialize
-  std::cout << "var2 : " << var2 << std::endl;
-  
-  var2 = 99.99; // Assign
-  std::cout << "var2 : " << var2 << std::endl;
-  
-  std::cout << std::endl;
+  //Subtraction
+  result = number2 - number1 ;
+  std::cout << "result : " << result << std::endl;
 
-  std::cout << "----------------" << std::endl;
+  result = number1 - number2;
+  std::cout << "result : " << result << std::endl;
 
-  std::cout << std::endl;
-  bool state{false}; // Declare and initialize
-  std::cout << std::boolalpha;
-  std::cout << "state : " << state << std::endl;
-  
-  state = true; // Assign
-  
-  std::cout << "state : " << state << std::endl;
+  //Multiplication
+  result = number1 * number2;
+  std::cout << "result : " << result << std::endl;
 
-  //Auto type deduction
-  //Careful about auto assignments
-  auto var3 {333u}; // Declare and initialize with type deduction
-  
-  var3 = -22; // Assign negative number. DANGER!
-  
-  std::cout << "var3 : " << var3 << std::endl;
+
+  //Division
+  result = number2 / number1;
+  std::cout << "result : " << result << std::endl;
+
+
+  //Modulus
+  result = number2 % number1; // 7 % 2
+  std::cout << "result : " << result << std::endl; // 1
+
+  result = 31 % 10;
+  std::cout << "result : " << result << std::endl; // 1
 
   return 0;
 }
