73cfc9a9ccdc69212e2111c393170a196e41b52a
Alin Cristian <alin.cristian977@gmail.com>
2023-09-18 18:04:38 +0300

    Compound Operators

--- main.cpp
+++ main.cpp
@@ -1,69 +1,33 @@
-// Prefix And Postfix Plus and Minus
+// Compound Operators
 
 #include <iostream>
 
 int main() {
 
-	int value { 5 };
-    
-  //Increment by one
-  value = value + 1; //6
-  std::cout << "The value is : " << value << std::endl; // 6
+  int value {45};
   
-  value = 5; // Reset value to 5
-  
-  //Decrement by one
-  value = value - 1; // 4
-  std::cout << "The value is : " << value << std::endl; //4
-  
-  //===================================================================
-
-  
-  std::cout << "======Postfix increment and decrement======"<< std::endl;
-  //Reset value to 5
-  value = 5;
-  
-  std::cout << "The value is (incrementing) : " << value++ << std::endl; // 5
-  std::cout << "The value is : " << value << std::endl; // 6
+  std::cout << "The value is : " << value << std::endl;
   
   std::cout << std::endl;
-  
-  //Decrement with postfix
-  
-  //Reset value to 5
-  value = 5;
-  
-  std::cout << "The value is (decrementing) : " << value-- << std::endl; //5
-  std::cout << "The value is : " << value << std::endl; // 4
+  value = value + 5;
+  //value +=5; // equivalent to value = value + 5
+  std::cout << "The value is (after +=5) : " << value << std::endl; // 50
 
-
-  //===================================================================
-
-  
-  std::cout << "======Prefix increment and decrement======"<< std::endl;
-
-  //Reset value to 5
-  value = 5;
-  
-  ++value;
-  std::cout << "The value is (prefix++) : " << value << std::endl; // 6
-  
-  //Reset value to 5
-  value = 5;
-  std::cout << "The value is (prefix++ in place) : " << ++value << std::endl; // 6
-  
   std::cout << std::endl;
-  
-  //Prefix : Decrementing
-  
-  //Reset value to 5;
-  value = 5;
-  --value;
-  std::cout << "The value is (prefix--) : " << value << std::endl; // 4
-  
-  //Reset value to 5;
-  value = 5;
-  std::cout << "The value is (prefix-- in place) : " << --value << std::endl;//4
+  value -=5; // equivalent to value = value - 5
+  std::cout << "The value is (after -=5) : " << value << std::endl; // 45 
+
+  std::cout << std::endl;
+  value *=2;
+  std::cout << "The value is (after *=2) : " << value << std::endl; // 90
+
+  std::cout << std::endl;
+  value /= 3;
+  std::cout << "The value is (after /=3) : " << value << std::endl; // 30
+
+  std::cout << std::endl;
+  value %= 11;
+  std::cout << "The value is (after %=11) : " << value << std::endl;// 8
 
   return 0;
 }
