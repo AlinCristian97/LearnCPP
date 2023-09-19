aa21ddb4668e5aa215cc3efe40de2a2d4722e543
Alin Cristian <alin.cristian977@gmail.com>
2023-09-19 08:22:20 +0300

    If Statement

--- main.cpp
+++ main.cpp
@@ -1,25 +1,107 @@
-// Weird Integral Types
+// If Statement
 
 #include <iostream>
 
 int main() {
 
-	short int var1 {10}; // 2 bytes
-	short int var2 {20};
+	int number1 {75};
+	int number2 {60};
+	bool result = (number1 < number2);//Expression yielding the condition
+    
+	/*
 	
-	char var3 {40}; //1 byte
-	char var4 {50};
+	std::cout << std::boolalpha << "result : " << result << std::endl; 
 	
-	std::cout << "size of var1 : " << sizeof(var1) << std::endl;
-	std::cout << "size of var2 : " << sizeof(var2) << std::endl;
-	std::cout << "size of var3 : " << sizeof(var3) << std::endl;
-	std::cout << "size of var4 : " << sizeof(var4) << std::endl;
-	
-	auto result1 = var1 + var2 ;
-	auto result2 = var3 + var4;
-	
-	std::cout << "size of result1 : " << sizeof(result1) << std::endl; // 4
-	std::cout << "size of result2 : " << sizeof(result2) << std::endl; // 4
+	std::cout << std::endl;
+	std::cout << "free standing if statement" << std::endl;
+   
+	//if(result){
+	if(result == true){
+		std::cout << number1 << " is less than " << number2 << std::endl;
+	}
+    
+	//if(!result){
+	if(!(result == true)){
+		std::cout << number1 << " is NOT less than " << number2 << std::endl;
+	}
 
-  return 0;
+	*/
+
+
+	//Using else
+	/*
+	std::cout << std::endl;
+	std::cout << "using the else clause : " << std::endl;
+	
+	if(result == true){
+		std::cout << number1 << " is less than " << number2 << std::endl;
+	} else {
+		std::cout << number1 << " is NOT less than " << number2 << std::endl;
+	}
+	*/
+
+
+	//Use expression as condition directly
+    /*
+	std::cout << std::endl;
+	std::cout << "Using expression as condition : " << std::endl;
+	
+	if(number1 < number2){
+		std::cout << number1 << " is less than " << number2 << std::endl;
+	}else{
+		std::cout << number1 << " is NOT less than " << number2 << std::endl;
+	}
+	*/
+
+
+	//Nesting if statements
+	std::cout << std::endl;
+	std::cout << "Nesting if statements" << std::endl;
+	
+	bool red = false;
+	bool green {true};
+	bool yellow {false};
+	bool police_stop{true};
+    
+	/*
+	*      If green : go
+	*      If red, yellow : stop
+	*      If green and police_stop : stop
+	* */
+	/*
+	if(red){
+			std::cout << "Stop" << std::endl;
+	}
+	if(yellow){
+			std::cout << "Slow down" << std::endl;
+	}
+	 if(green){
+		 std::cout << "Go" << std::endl;
+	 }
+     */
+
+    /*
+	std::cout << std::endl;
+	 std::cout << "Police officer stops(verbose)" << std::endl;
+	 if(green){
+			if(police_stop){
+					std::cout << "Stop" << std::endl;
+			}
+			else{
+					std::cout << "Go" << std::endl;
+			}
+		}
+		*/
+
+
+	std::cout << std::endl;
+	std::cout << "Police officer stops(less verbose)" << std::endl;
+
+	if(green && !police_stop) {
+	std::cout << "Go" << std::endl;
+	} else {
+	std::cout << "Stop" << std::endl;
+	}
+
+	return 0;
 }
