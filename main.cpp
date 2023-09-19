ca5497f2fce0b917629e4857ef1d475aa938992f
Alin Cristian <alin.cristian977@gmail.com>
2023-09-19 08:34:45 +0300

    Ternary Operator

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Switch
+// Ternary Operator
 
 #include <iostream>
 
@@ -13,53 +13,25 @@ const int Ellipse{60};
 int main()
 {
 
-	int tool{Eraser};
+	int max{};
+    
+	int a{35};
+	int b{200};
 
-	switch (tool)
-	{
-	case Pen:
-	{
-		std::cout << "Active tool is Pen" << std::endl;
-	}
-	break;
-
-	case Marker:
-	{
-		std::cout << "Active tool is Marker" << std::endl;
-	}
-	break;
-
-	case Eraser:
-	case Rectangle:
-	case Circle:
-	{
-		std::cout << "Drawing Shapes" << std::endl;
-	}
-	break;
-
-	case Ellipse:
-	{
-		std::cout << "Active tool is Ellipse" << std::endl;
-	}
-	break;
-
-	default:
-	{
-		std::cout << "No match found" << std::endl;
-	}
-	break;
-	}
-
-	std::cout << "Moving on" << std::endl;
-
+	std::cout << std::endl;
+	std::cout << "using regular if " << std::endl;
+	
 	/*
-	// Condition can only be integer of enum (We'll learn about enums later in the course)
-	std::string name {"John"};
-	switch (name) // Compiler error!
-	{
-
+	if(a >  b){
+		max = a;
+	}else{
+		max = b;
 	}
 	*/
+
+	max = (a > b)? a : b; // Ternary operator
+
+	std::cout << "max : " << max << std::endl;
 
 	return 0;
 }
