2b6ae90a97126b44bf9a6f7d448cb9279c2347cb
Alin Cristian <alin.cristian977@gmail.com>
2023-09-19 20:13:02 +0300

    Bounds of an Array

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Arrays Of Characters
+// Bounds Of An Array
 
 #include <iostream>
 
@@ -6,77 +6,20 @@
 int main()
 {
 
-	//Declare array
-    char message [5]  {'H', 'e', 'l', 'l', 'o'};
-    //int data[5] {1,2,3,3,3};
+	int numbers[] {1,2,3,4,5,6,7,8,9,0};
+	
+	//Read beyond bounds : Will read garbage or crash your program
+	//std::cout << "numbers[12] : " << numbers[12] << std::endl;
 
-    //std::cout << "data : " << data << std::endl;
-    
-  
-    //Print out the array through looping
-    /*
-    std::cout << "message : ";
-    for( auto c : message){
-        std::cout << c ;
-    }
-    std::cout << std::endl;
-    */
+	//Write beyond bounds. The compiler allows it. But you don't own 
+	//the memory at index 12, so other programs may modify it and your
+	//program may read bogus data at a later time. Or you can even
+	//corrupt data used by other parts of your program
 
-    
-   /*
-    //Change characters in our array
-    message[1] = 'a';
+	numbers[129999999] = 1000;
+	std::cout << "numbers[129999999] : " << numbers[129999999] << std::endl;
 
-    //Print out the array through looping
-    std::cout << "message : ";
-    for( auto c : message){
-        std::cout << c ;
-    }
-    std::cout << std::endl;
-    */
-
-   
-   
-    // Will probably print garbage after your char array
-    /*
-    std::cout << "message : " << message << std::endl;
-    */
-   
-
- 
-    //If a character array is null terminated, it's called as C-String
-    /*
-    char message1 [] {'H','e','l','l','o','\0'};
-    std::cout << "message1 : " << message1 << std::endl;
-    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl;
-    */
-
-  
-   /*
-    char message2 [6] {'H','e','l','l','o'};
-    std::cout << "message2 : " << message2 << std::endl;
-    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl;
-
-    char message3 [] {'H','e','l','l','o'}; // This is not a c string ,
-											//as there is not null character
-    std::cout << "message3 : " << message3 << std::endl;
-    std::cout << "sizeof(message3) : " << sizeof(message3) << std::endl;
-    */
-
-    
-
-    //String literal
-    /*
-    char message4 [] {"Hello"};
-    std::cout << "message4 : " << message4 << std::endl;
-    std::cout << "sizeof(message4) : " << sizeof(message4) << std::endl;
-    */
-
-
-
-    //Can't safely print out arrays other than those of characters
-    int numbers [] {1,2,3,4,5};
-    std::cout << "numbers :  " << numbers << std::endl;
+	std::cout << "Program ending...." << std::endl;
 
 	return 0;
 }
