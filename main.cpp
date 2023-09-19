5fb7ad0251870f9e7c43de5ab458dfbb4912a632
Alin Cristian <alin.cristian977@gmail.com>
2023-09-20 00:36:04 +0300

    Program Memory Map

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Pointer to Char
+// Program Memory Map
 
 #include <iostream>
 
@@ -6,16 +6,7 @@
 int main()
 {
 
-	const char* message {"Hello World!"};
-	std::cout << "message : " << message << std::endl;
-
-	//*message = "B"; // Compiler error
-	std::cout << "*message : " << *message << std::endl; // dereference
-
-	//Allow users to modify the string
-	char message1[] {"Hello World!"};
-	message1[0] = 'B';
-	std::cout << "message1 : " << message1 << std::endl;
+	
 
 	return 0;
 }
