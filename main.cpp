d970f6a30ad33e0934fa311d15a0ab74437e41a1
Alin Cristian <alin.cristian977@gmail.com>
2023-09-20 15:44:20 +0300

    Declaring and Using References

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Dynamic Arrays
+// Declaring and Using References
 
 #include <iostream>
 
@@ -6,59 +6,62 @@
 int main()
 {
 
-    const size_t size{10};
-    
-    //Different ways you can declare an array
-	//dynamically and how they are initialized
-	
-    double *p_salaries { new double[size]}; // salaries array will
-															//contain garbage  values
-    int *p_students { new(std::nothrow) int[size]{} }; // All values initialized to 0 
-    
-    double *p_scores { new(std::nothrow) double[size]{1,2,3,4,5}}; // Allocating memory space
-																	// for an array  of size double
-																	//vars. First 5 will be initialized
-																	//with 1,2,3,4,5, and the 
-																	//rest will be 0's.
+    int int_data{33};
+    double double_data{55};
+
+    //References
+    int& ref_int_data{int_data};
+    double& ref_double_data{double_data};
+
+    //Print stuff out
+    std::cout << "int_data : " << int_data << std::endl;
+    std::cout << "&int_data : " << &int_data << std::endl;
+    std::cout << "double_data : " << double_data << std::endl;
+    std::cout << "&double_data : " << &double_data << std::endl;
+
+    std::cout << "=======================" << std::endl;
+
+    std::cout << "ref_int_data : " << ref_int_data << std::endl;
+    std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
+    std::cout << "ref_double_data : " << ref_double_data << std::endl;
+    std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
+
+    int_data = 111;
+    double_data = 67.2;
 
 
-    //nullptr check and use the allocated array
-    if(p_scores){
-        std::cout << "size of scores (it's a regular pointer) : " << sizeof(p_scores) << std::endl;
-        std::cout << "Successfully allocated memory for scores."<< std::endl;
-        
-        //Print out elements. Can use regular array access notation, or pointer arithmetic
-        for( size_t i{}; i < size ; ++i){
-           std::cout << "value : " << p_scores[i] << " : " << *(p_scores + i) << std::endl; 
-        }
-    }
+    //Print stuff out
+    std::cout << std::endl;
+    std::cout << "int_data : " << int_data << std::endl;
+    std::cout << "&int_data : " << &int_data << std::endl;
+    std::cout << "double_data : " << double_data << std::endl;
+    std::cout << "&double_data : " << &double_data << std::endl;
 
-    delete [] p_salaries;
-    p_salaries = nullptr;
+    std::cout << "=======================" << std::endl;
 
-    delete [] p_students;
-    p_students = nullptr;
+    std::cout << "ref_int_data : " << ref_int_data << std::endl;
+    std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
+    std::cout << "ref_double_data : " << ref_double_data << std::endl;
+    std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
 
-   delete [] p_scores;
-   p_scores = nullptr;
 
-   //Static arrays Vs dynamic arrays
-   std::cout << "=====================================" << std::endl;
+    ref_int_data = 1012;
+    ref_double_data = 1000.45;
 
-   int scores[10] {1,2,3,4,5,6,7,8,9,10}; // Lives on the stack
 
-   std::cout << "scores size : " << std::size(scores) << std::endl;
-   for( auto s : scores){
-       std::cout << "value : " << s << std::endl;
-   }
+   //Print stuff out
+    std::cout << std::endl;
+    std::cout << "int_data : " << int_data << std::endl;
+    std::cout << "&int_data : " << &int_data << std::endl;
+    std::cout << "double_data : " << double_data << std::endl;
+    std::cout << "&double_data : " << &double_data << std::endl;
 
-   int* p_scores1 = new int[10] {1,2,3,4,5,6,7,8,9,10}; // Lives on the heap.
-   //std::cout << "p_scores1 size : " << std::size(p_scores) << std::endl;
-   /*
-   for( auto s : p_scores1){
-       std::cout << "value : " << s << std::endl;
-   }
-   */
+    std::cout << "=======================" << std::endl;
+
+    std::cout << "ref_int_data : " << ref_int_data << std::endl;
+    std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
+    std::cout << "ref_double_data : " << ref_double_data << std::endl;
+    std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
 
 	return 0;
 }
