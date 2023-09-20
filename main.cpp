4bac41209611a59954fa3756e183a1522cd79f2f
Alin Cristian <alin.cristian977@gmail.com>
2023-09-20 16:26:17 +0300

    References and Const

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Comparing Pointers and References
+// References and Const
 
 #include <iostream>
 
@@ -6,72 +6,30 @@
 int main()
 {
 
-    //Declare pointer and reference
-
-    double double_value {12.34};
-    double& ref_double_value {double_value}; // Reference to double_value
-    double* p_double_value {&double_value}; //Pointer to double_value
+    //Non const reference
+	std::cout << std::endl;
+	std::cout << "Non const reference : " << std::endl;
+	int age {27};
+	const int& ref_age{age};
+	/*
+	std::cout << "age : " << age << std::endl;
+	std::cout << "ref_age : " << ref_age << std::endl;
 	
-	//Reading
-	std::cout << "double_value : " << double_value << std::endl;
-	std::cout << "ref_double_value : " << ref_double_value << std::endl;
-	std::cout << "p_double_value : " << p_double_value << std::endl;
-	std::cout << "*p_double_value : " << *p_double_value << std::endl;
 	
-	//Writting through pointer
-	*p_double_value = 15.44;
+	//Can modify original variable through reference
+
+	std::cout << std::endl;
+	std::cout << "Modify original variable through reference : " << std::endl;
 	
-    std::cout << std::endl;
-	std::cout << "double_value : " << double_value << std::endl;
-	std::cout << "ref_double_value : " << ref_double_value << std::endl;
-	std::cout << "p_double_value : " << p_double_value << std::endl;
-	std::cout << "*p_double_value : " << *p_double_value << std::endl;
+	ref_age++; //Mofify through reference
 	
-	//Writting through reference
-	ref_double_value = 18.44;
-	
-    std::cout << std::endl;
-	std::cout << "double_value : " << double_value << std::endl;
-	std::cout << "ref_double_value : " << ref_double_value << std::endl;
-	std::cout << "p_double_value : " << p_double_value << std::endl;
-	std::cout << "*p_double_value : " << *p_double_value << std::endl;
+	std::cout << "age : " << age << std::endl;
+	std::cout << "ref_age : " << ref_age << std::endl;
+    */
 
-
-    double some_other_double{78.45};
-
-    //Make the reference reference something else.
-    ref_double_value = some_other_double;
-
-    std::cout << "Making the reference reference something else..." << std::endl;
-    std::cout << std::endl;
-	std::cout << "double_value : " << double_value << std::endl;
-	std::cout << "ref_double_value : " << ref_double_value << std::endl;
-	std::cout << "p_double_value : " << p_double_value << std::endl;
-	std::cout << "*p_double_value : " << *p_double_value << std::endl;
-
-
-
-    //Make the pointer point to something else
-    p_double_value = &some_other_double;
-    std::cout << "Making the pointer point somewhere else..." << std::endl;
-    std::cout << std::endl;
-	std::cout << "double_value : " << double_value << std::endl;
-	std::cout << "ref_double_value : " << ref_double_value << std::endl;
-    std::cout << "&double_value : " << &double_value << std::endl;
-    std::cout << "&ref_double_value : " << &ref_double_value << std::endl;
-	std::cout << "p_double_value : " << p_double_value << std::endl;
-	std::cout << "*p_double_value : " << *p_double_value << std::endl;
-
-
-    *p_double_value = 555.5;
-
-    std::cout << std::endl;
-	std::cout << "double_value : " << double_value << std::endl;
-	std::cout << "ref_double_value : " << ref_double_value << std::endl;
-    std::cout << "&double_value : " << &double_value << std::endl;
-    std::cout << "&ref_double_value : " << &ref_double_value << std::endl;
-	std::cout << "p_double_value : " << p_double_value << std::endl;
-	std::cout << "*p_double_value : " << *p_double_value << std::endl;
+   //Simulating reference behavior with pointers
+   //const int * const p_age {&age};
+   //*p_age = 45;
 
 	return 0;
 }
