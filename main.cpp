09b50e516d6c40102794d4f2df0bf50f7b91a43b
Alin Cristian <alin.cristian977@gmail.com>
2023-09-22 00:18:35 +0300

    Declaring and using Lambda Functions

--- main.cpp
+++ main.cpp
@@ -1,50 +1,103 @@
-// Overloading with different parameters
+// Declaring and using Lambda Functions
 
 #include <iostream>
-
-int max(int a, int b){
-    std::cout << "int overload called" << std::endl;
-    return (a>b)? a : b; 
-}
-
-double max(double a, double b){
-    std::cout << "double overload called" << std::endl;
-    return (a>b)? a : b; 
-}
-
-double max(int a, double b){
-    std::cout << "(int,double) overload called" << std::endl;
-    return (a>b)? a : b; 
-}
-
-double max(double a, int b){
-    std::cout << "(double,int) overload called" << std::endl;
-    return (a>b)? a : b; 
-}
-
-double max(double a, int b,int c){
-    std::cout << "(double,int,int) overload called" << std::endl;
-    return a;
-}
-
-std::string_view  max(std::string_view a, std::string_view b){
-    std::cout << "(string_view,string_view) overload called" << std::endl;
-     return (a>b)? a : b;
-}
 
 //TODO: Return to (https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/) to see the extra sections, if they are not already discussed
 int main()
 {
 
-    int x{4};
-    int y{9};
-    double a{5.4};
-    double b{7.4};
+	/*
+	Lambda function signature : 
+															[capture list] (parameters) ->return type{
+																			// Function body
+															}
+	*/
 
-    auto result = max(b,x,y);
+	//Declaring a lambda function and calling it through a name
+	/*
+	auto func = [](){
+			std::cout << "Hello World!" << std::endl;
+	};
+	func();
+	func();
+	*/
 
-    max("Hello","World");
+
+	//Declare a lambda function and call it directly
+	/*
+	[](){
+			std::cout << "Hello World!" << std::endl;
+	}();
+	*/
+
+
+	//Lambda function that takes parameters
+
+	/*
+	[](double a, double b){
+			std::cout << "a + b : " << (a + b)  << std::endl;
+	}(10.0,50.0);
+	*/
+
+	/*
+	auto func1 = [](double a, double b){
+			std::cout << "a + b : " << (a + b)  << std::endl;
+	};
+
+	func1(10,20);
+	func1(5,7);
+	*/
+
+
+	//Lambda function that returns something
+	/*
+	auto result = [](double a, double b){
+			return a + b;
+	}(10,60);
+
+	//std::cout << "result : " << result << std::endl;
+	std::cout << "result : " << [](double a, double b){
+			return a + b;
+	}(10,60) << std::endl;
+	*/
+
+
+	/*
+	auto func1 = [](double a, double b){
+			return a + b;
+	};
+
+	auto result1 = func1(23,7);
+	auto result2 = func1(9,45);
+
+	std::cout << "result1 : " << result1 << std::endl;
+	std::cout << "result2 : " << result2 << std::endl;
+	std::cout  <<"direct call : " << func1(5,2) << std::endl;
+	*/
+
+	//Explicitly specify the return type
+	auto func3 = [](double a, double b)-> int{
+			return a + b;
+	};
+
+	auto func4 = [](double a, double b){
+			return a + b;
+	};
+
+	double a{6.9};
+	double b{3.5};
+
+	auto result3 = func3(a,b);
+	auto result4 = func4(a,b);
+
+	std::cout << "result3 : " << result3 << std::endl;
+	std::cout << "result4 : " << result4 << std::endl;
+	std::cout << "sizeof(result3) : " << sizeof(result3) << std::endl; // 4
+	std::cout << "sizeof(result4) : " << sizeof(result4) << std::endl; // 8
+
+
+	std::cout << "Done!" << std::endl;
    
-    return 0;
+	return 0;
 
 }
