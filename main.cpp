294a6ea508ab92f18b172db3cf8df8d611c099d6
Alin Cristian <alin.cristian977@gmail.com>
2023-09-22 00:38:39 +0300

    Capture all in context

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Capture lists
+// Capture all in context
 
 #include <iostream>
 
@@ -6,45 +6,36 @@
 int main()
 {
 
-	//Capture lists
+	//Capture everything by value
 	/*
-		double a{10};
-		double b{20};
-		
-		auto func = [a,b](){
-				std::cout  << "a + b : " << a + b << std::endl;
-		};
-		func();
-		*/
+	int c{42};
+	
+	auto func = [=](){
+			std::cout << "Inner value : " << c << std::endl;
+	};
+	
+	for(size_t i{} ; i < 5 ;++i){
+			std::cout << "Outer value : " << c << std::endl;
+			func();
+			++c;
+	}
+	*/
 
-	//Capturing by value
-	/*
-		int c{42};
-		
-		auto func = [c](){
-				std::cout << "Inner value : " << c << " &inner : " <<&c <<  std::endl;
-		};
-		
-		for(size_t i{} ; i < 5 ;++i){
-				std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
-				func();
-				++c;
-		}
 
-		*/
-
-	//Capture by reference
-		int c{42};
-		
-		auto func = [&c](){
-				std::cout << "Inner value : " << c << " &inner : " <<&c <<  std::endl;
-		};
-		
-		for(size_t i{} ; i < 5 ;++i){
-				std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
-				func();
-				++c;
-		}
+	//Capturing all reference
+	int c{42};
+	int d{5};
+	
+	auto func = [&](){
+			std::cout << "Inner value : " << c << std::endl;
+			std::cout << "Inner value(d) : " << d << std::endl;
+	};
+	
+	for(size_t i{} ; i < 5 ;++i){
+			std::cout << "Outer value : " << c << std::endl;
+			func();
+			++c;
+	}
 
 	return 0;
 
