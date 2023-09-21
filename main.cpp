716f08cdbd68844f8b93bfc2b1a82fcc010d0ab6
Alin Cristian <alin.cristian977@gmail.com>
2023-09-21 18:20:06 +0300

    Pass by reference

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-// Pass by pointer
+// Pass by reference
 
 #include <iostream>
 
-void say_age(int* age); // Declaration
+void say_age(int& age); // Declaration
 
 //TODO: Return to (https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/) to see the extra sections, if they are not already discussed
 int main()
@@ -11,7 +11,7 @@ int main()
     int age{23}; // Local
     std::cout << "age (before call) : " << age << "&age : " << &age << std::endl; //23
 
-    say_age(&age); // Argument
+    say_age(age); // Argument
 
     std::cout << "age (after call) : " << age << "&age : " << &age <<  std::endl; //24
 
@@ -19,7 +19,7 @@ int main()
 
 }
 
-void say_age(int* age){ // Parameter
-    ++(*age); 
-    std::cout <<  "Hello , you are " << *age << " years old! &age : " << &age <<  std::endl; //24
+void say_age(int& age) { // Parameter
+    ++age; 
+    std::cout <<  "Hello , you are " << age << " years old! &age : " << &age <<  std::endl; //24
 }
