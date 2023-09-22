383d0d1290eca80e4d755e5737b9e2dc0fb86ff3
Alin Cristian <alin.cristian977@gmail.com>
2023-09-23 00:50:27 +0300

    First C++ Class

--- main.cpp
+++ main.cpp
@@ -1,19 +1,42 @@
-// Concepts and auto
+// First C++ Class
 
 #include <iostream>
-#include <concepts>
 
-//This syntax constrains the auto parameters you pass in 
-//to comply with the std::integral concept
-std::integral auto add (std::integral auto a,std::integral auto b){
-	return a + b;
-}
+const double PI {3.1415926535897932384626433832795};
+
+// Blueprint
+class Cylinder {
+
+	public:
+		// Functions (methods)
+		double volume() {
+			return PI * baseRadius * baseRadius * height;
+		}
+
+	public:
+		// Member variables
+		double baseRadius {1};
+		double height {1};
+
+};
 
 //TODO: Return to (https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/) to see the extra sections, if they are not already discussed
 int main()
 {
 
-	std::floating_point auto x = add(5,8);
+	Cylinder cylinder1; // Objects
+	
+	std::cout << "volume: " << cylinder1.volume() << std::endl;
+
+	// Change the member varialbes
+	cylinder1.baseRadius = 10;
+	cylinder1.height = 3;
+
+	std::cout << "volume: " << cylinder1.volume() << std::endl;
+
+	cylinder1.height = 8;
+
+	std::cout << "volume: " << cylinder1.volume() << std::endl;
 
 	return 0;
 
