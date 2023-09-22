f6f958bd774374cc056023a3dd84fd11603b09b9
Alin Cristian <alin.cristian977@gmail.com>
2023-09-23 00:05:02 +0300

    Building your own Concepts

--- main.cpp
+++ main.cpp
@@ -1,35 +1,36 @@
-// Concepts
+// Building your own Concepts
 
 #include <iostream>
+#include <type_traits>
 #include <concepts>
 
 //Syntax1
 /*
 template <typename T>
-requires std::integral<T>
-T add( T a, T b){
+concept MyIntegral = std::is_integral_v<T>;
+
+MyIntegral auto add( MyIntegral auto a, MyIntegral auto b) {
     return a + b;
 }
 */
 
-//Syntax2
-/*
-template <std::integral T>
-T add( T a, T b){
-    return a + b;
-}
-*/
 
-//Syntax3
-/*
-auto add(std::integral auto a , std::integral auto b){
-    return a + b;
-}
-*/
-
-//Syntax4
 template <typename T>
-T add( T a, T b) requires std::integral<T>{
+concept Multipliable =  requires(T a, T b) {
+	a * b; // Just makes sure the syntax is valid
+};
+
+
+template <typename T>
+concept Incrementable = requires (T a) {
+	a+=1;
+	++a;
+	a++;
+};
+
+template <typename T>
+requires Incrementable<T>
+T add (T a, T b){
     return a + b;
 }
 
@@ -37,22 +38,15 @@ T add( T a, T b) requires std::integral<T>{
 int main()
 {
 
-	char a_0{10};
-	char a_1{20};
-	
-	auto result_a = add(a_0,a_1);
-	std::cout << "result_a : " << static_cast<int>(result_a) << std::endl;
-	
-	int b_0{11};
-	int b_1{5};
-	auto result_b = add(b_0,b_1);
-	std::cout << "result_b : " << result_b << std::endl;
+	double x{6};
+	double y{7};
 
+	//std::string x{"Hello"};
+	//std::string y{"World"};
 
-	double c_0 {11.1};
-	double c_1 {1.9};
-	auto result_c = add(c_0,c_1);
-    std::cout << "result_c : " << result_c << std::endl;
+	add(x,y);
+
+	std::cout << "Done!" << std::endl;
 
 	return 0;
 
