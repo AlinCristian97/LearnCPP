54023bbe8e731cef5668725814caf6d69e853db1
Alin Cristian <alin.cristian977@gmail.com>
2023-09-23 00:18:42 +0300

    Concepts and auto

--- main.cpp
+++ main.cpp
@@ -1,35 +1,19 @@
-// Logical combinations of Concepts
+// Concepts and auto
 
 #include <iostream>
 #include <concepts>
 
-
-template <typename T>
-concept TinyType = requires ( T t){
-	sizeof(T) <=4; // Simple requirement
-	requires sizeof(T) <= 4; // Nested requirement
-};
-
-
-template <typename T>
-//requires std::integral<T> || std::floating_point<T> // OR operator
-//requires std::integral<T> && TinyType<T>
-requires std::integral<T> && requires ( T t){
-	sizeof(T) <=4; // Simple requirement
-	requires sizeof(T) <= 4; // Nested requirement
-}
-T add(T a, T b){
-    return a + b;
+//This syntax constrains the auto parameters you pass in 
+//to comply with the std::integral concept
+std::integral auto add (std::integral auto a,std::integral auto b){
+	return a + b;
 }
 
 //TODO: Return to (https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/) to see the extra sections, if they are not already discussed
 int main()
 {
 
-	long long int x{7};
-	long long int y{5};
-
-	add(x,y);
+	std::floating_point auto x = add(5,8);
 
 	return 0;
 
