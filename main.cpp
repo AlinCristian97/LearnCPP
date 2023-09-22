7585672f3b4a79f40082c10b6eb75cb1ce363a00
Alin Cristian <alin.cristian977@gmail.com>
2023-09-23 00:59:31 +0300

    Default Constructors

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Constructors
+// Default Constructors
 
 #include <iostream>
 
@@ -6,24 +6,20 @@ const double PI {3.1415926535897932384626433832795};
 
 class Cylinder {
     public : 
-        // Constructors
-        Cylinder(){
-            base_radius = 2.0;
-            height = 2.0;
-        }
-
+        //Constctors
+        Cylinder() = default;
         Cylinder(double rad_param,double height_param){
             base_radius = rad_param;
             height = height_param;
         }
    
-        // Functions (methods)
+        //Functions (methods)
         double volume(){
             return PI * base_radius * base_radius * height;
         }
 
     private : 
-        // Member variables
+        //Member variables
         double base_radius{1};
         double height{1};
 };
@@ -32,7 +28,6 @@ class Cylinder {
 int main()
 {
 
-	//Cylinder cylinder1(10,4); // Object
 	Cylinder cylinder1;
 	std::cout << "volume : " << cylinder1.volume() << std::endl;
 
