270a6d1448ce7a6f082cadd40f3f4f5bff4754cc
Alin Cristian <alin.cristian977@gmail.com>
2023-09-27 16:48:01 +0300

    Don't Call Virtual (Polymorphic) Functions From Constructors & Destructors

--- main.cpp
+++ main.cpp
@@ -1,93 +1,68 @@
-// Dynamic Casts
+// Don't Call Virtual (Polymorphic) Functions From Constructors & Destructors
 
 #include <iostream>
-#include "dog.h"
 
+class Base
+{
+public:
+    Base()
+    {
+        std::cout << "Base constructor called" << std::endl;
+    }
+    virtual ~Base(){
+        std::cout << "Base destructor called" << std::endl;
+    }
+    virtual void setup()
+    {
+        std::cout << "Base::setup() called" << std::endl;
+        m_value = 10;
+    }
+    virtual void clean_up(){
+        std::cout << "Base::clean_up() called" << std::endl;
+    }
+    int get_value()
+    {
+        return m_value;
+    }
+protected:
+    int m_value;
+};
 
-void do_something_with_animal_ptr(Animal* animal){
-	std::cout<< "In function taking base pointer..." << std::endl;
-	Feline* feline_ptr = dynamic_cast<Feline*>(animal);
-	
-	if(feline_ptr){
-		feline_ptr->do_some_feline_thingy();
-	}else{
-		std::cout << "Couldn't cast to Feline pointer,Sorry." << std::endl;
-	}
-	
-}
-
-void do_something_with_animal_ref(Animal & animal){
-	std::cout << "In function taking base reference..." << std::endl;
-	Feline* feline_ptr_1 {dynamic_cast<Feline*>(&animal)};
-	if(feline_ptr_1){
-		feline_ptr_1->do_some_feline_thingy();
-	}else{
-		std::cout << "Couldn't cast to Feline reference,Sorry." << std::endl;
-	}
-}
-
-//TODO: Return to (https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/) to see the extra sections, if they are not already discussed
-int main(){
-
-	//Base class pointer
-	 Animal* animal1 = new Feline("stripes","feline1");
-	 //animal1->do_some_feline_thingy();
-
-
-    std::cout << "-----------" << std::endl;
-
-	//If the cast succeeds, we get a valid pointer back, 
-	//if it fails, we get nullptr. So we can check before
-	//calling stuff on the returned pointer
-	Feline* feline_ptr = dynamic_cast<Feline*>(animal1);
-
-    if(feline_ptr){
-        feline_ptr->do_some_feline_thingy();
-    }else{
-        std::cout << "Couldn't do the transformation from Animal* to Dog*" << std::endl;
+class Derived : public Base
+{
+public:
+    Derived()
+        : Base()
+    {
+        std::cout << "Derived constructor called" << std::endl;
+    }
+    virtual ~Derived(){
+        std::cout << "Derived destructor called" << std::endl;
     }
 
-    std::cout << "-----------" << std::endl;
+    virtual void setup() override
+    {
+        std::cout << "Derived::setup() called" << std::endl;
+        m_value = 100;
+    }
+    virtual void clean_up() override{
+        std::cout << "Derived::clean_up() called" << std::endl;
+    }
+};
 
-    //Can do the transformation downstream for references
+//TODO: Return to (https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/) to see the extra sections, if they are not already discussed
+int main() {
 
-	//Base class reference
-	Feline feline2 ("stripes", "feline2");
-	Animal & animal_ref = feline2;
-	
-	//Calling non virtual methods on animal_ref won't work
-	//animal_ref.do_some_feline_thingy();
-	
-	//Dog & feline_ref {dynamic_cast<Dog&>(animal_ref)};
-	//feline_ref.do_some_dog_thingy();
+    Base * p_base = new Derived;
 
+    p_base->setup();
 
-    //Doing proper checks with references
-	Dog* feline_ptr_1 {dynamic_cast<Dog*>(&animal_ref)};
-	if(feline_ptr_1){
-		feline_ptr_1->do_some_dog_thingy();
-	}else{
-		std::cout << "Couldn't cast to Dog reference,Sorry." << std::endl;
-	}
- 
+    auto value = p_base->get_value();
+    std::cout << "value : " << value <<  std::endl; // 100
 
+    p_base->clean_up();
 
-    std::cout << "---------------" << std::endl;
-    do_something_with_animal_ptr(animal1);
-    do_something_with_animal_ref(animal_ref);
-
-
-    std::cout << "----------------" << std::endl;
-    /*
-    int data{45};
-    int data_ptr = &data;
-
-    std::string data_str = dynamic_cast<std::string *>(data_ptr);
-    */
-
-    std::cout << "Done!" << std::endl;
-
-    delete animal1;
+    delete p_base;
 
     return 0;
 
