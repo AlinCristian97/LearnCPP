10c51c971c38cf6a1e2c22df40b55fd7481842bc
Alin Cristian <alin.cristian977@gmail.com>
2023-09-27 14:58:44 +0300

    Overloading, Overriding And Hiding

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Override
+// Overloading, Overriding And Hiding
 
 #include <iostream>
 #include <memory>
@@ -8,23 +8,10 @@
 
 //TODO: Return to (https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/) to see the extra sections, if they are not already discussed
 int main(){
+    
+    Shape * shape_ptr = new Circle(10,"Circle1");
+    shape_ptr->draw(45,"Red");
 
-    Circle circle1(7.2,"circle1");
-    Oval oval1(13.3,1.2,"Oval1");
-    Circle circle2(11.2,"circle2");
-    Oval oval2(31.3,15.2,"Oval2");
-    Circle circle3(12.2,"circle3");
-    Oval oval3(53.3,9.2,"Oval3");
-
-    //Raw pointers
-  
-	Shape* shapes3[] {&circle1,&oval1,&circle2,&oval2,&circle3,&oval3};
-	
-	for ( Shape* shape_ptr : shapes3){
-		shape_ptr->draw();	
-		std::cout << std::endl;
-	}
-   
     return 0;
 
 }
