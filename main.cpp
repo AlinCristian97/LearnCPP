bfd9bc2cc5bdbe299ce619e3dfd505728db9dc8a
Alin Cristian <alin.cristian977@gmail.com>
2023-09-27 12:32:00 +0300

    Size of polymorphic objects and slicing

--- main.cpp
+++ main.cpp
@@ -1,66 +1,23 @@
-// Polymorphism with Virtual Functions
+// Size of polymorphic objects and slicing
 
 #include <iostream>
 #include "shape.h"
 #include "oval.h"
 #include "circle.h"
 
-void draw_shape(Shape * s){
-    s->draw();
-}
-
-void draw_shape_v1(const Shape& s_r){
-    s_r.draw();
-}
-
 //TODO: Return to (https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/) to see the extra sections, if they are not already discussed
 int main(){
 
-    Shape shape1("Shape1");
-    //shape1.draw();
+    //Comparing object sizes
+    std::cout << "sizeof(Shape) : " << sizeof(Shape) << std::endl; // dynamic : 40
+    std::cout << "sizeof(Oval) : " << sizeof(Oval) << std::endl; // dynamic : 56
+    std::cout << "sizeof(Circle) : " << sizeof(Circle) << std::endl; // dynamic : 56
 
-    Oval oval1(2.0,3.5,"Oval1");
-    //oval1.draw();
 
+    //Slicing
     Circle circle1(3.3,"Circle1");
-    //circle1.draw();
-
-
-
-    //Base pointers
-    Shape * shape_ptr = &shape1;
-    //shape_ptr->draw(); // Shape::draw
-
-    shape_ptr = &oval1;
-    //shape_ptr->draw(); // Oval::draw()
-
-    shape_ptr = &circle1;
-    //shape_ptr->draw(); // Circle::draw()
-
-
-    //Base references
-    Shape& shape_ref = circle1;
-    //shape_ref.draw(); // Cicle::draw()
-
-
- 
-    //Drawing shapes
-    //draw_shape(&circle1);
-    //draw_shape_v1(circle1);
-
-
-    //Raw pointers
-    shape_ptr  = &oval1;
-    //shape_ptr->get_x_rad();
-
-
-    //Shapes stored in collections
-    Shape* shape_collection[]{&shape1,&oval1,&circle1};
-	 
-	 for(Shape* s_ptr : shape_collection){
-        s_ptr->draw();
-    }
-    
+    Shape shape = circle1;
+    shape.draw(); //Shape::draw
    
     return 0;
 
