adc31f461e8776c4846b371e888fa4060e37d63b
Alin Cristian <alin.cristian977@gmail.com>
2023-09-27 16:55:31 +0300

    Pure Virtual Functions And Abstract Classes

--- main.cpp
+++ main.cpp
@@ -1,68 +1,26 @@
-// Don't Call Virtual (Polymorphic) Functions From Constructors & Destructors
+// Pure Virtual Functions And Abstract Classes
 
 #include <iostream>
-
-class Base
-{
-public:
-    Base()
-    {
-        std::cout << "Base constructor called" << std::endl;
-    }
-    virtual ~Base(){
-        std::cout << "Base destructor called" << std::endl;
-    }
-    virtual void setup()
-    {
-        std::cout << "Base::setup() called" << std::endl;
-        m_value = 10;
-    }
-    virtual void clean_up(){
-        std::cout << "Base::clean_up() called" << std::endl;
-    }
-    int get_value()
-    {
-        return m_value;
-    }
-protected:
-    int m_value;
-};
-
-class Derived : public Base
-{
-public:
-    Derived()
-        : Base()
-    {
-        std::cout << "Derived constructor called" << std::endl;
-    }
-    virtual ~Derived(){
-        std::cout << "Derived destructor called" << std::endl;
-    }
-
-    virtual void setup() override
-    {
-        std::cout << "Derived::setup() called" << std::endl;
-        m_value = 100;
-    }
-    virtual void clean_up() override{
-        std::cout << "Derived::clean_up() called" << std::endl;
-    }
-};
+#include "circle.h"
+#include "rectangle.h"
 
 //TODO: Return to (https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/) to see the extra sections, if they are not already discussed
 int main() {
 
-    Base * p_base = new Derived;
+   //Shape * shape_ptr = new Shape; // Compiler error
 
-    p_base->setup();
+    const Shape * shape_rect = new Rectangle(10,10,"rect1");
+    double surface = shape_rect->surface();
+    std::cout << "dynamic type of shape_rect : " << typeid(*shape_rect).name() << std::endl;
+    std::cout << "The surface of shape rect is : " << surface << std::endl;
 
-    auto value = p_base->get_value();
-    std::cout << "value : " << value <<  std::endl; // 100
 
-    p_base->clean_up();
+    std::cout << "--------------" << std::endl;
 
-    delete p_base;
+    const Shape * shape_circle = new Circle(10,"circle1");
+    surface = shape_circle->surface();
+    std::cout << "dynamic type of shape_circle : " << typeid(*shape_circle).name() << std::endl;
+    std::cout << "The surface of the circle is : " << surface << std::endl;
 
     return 0;
 
